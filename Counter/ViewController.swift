7aa3735f79cc0958a9ca5112905ab5df8307e636
pudramusic <flyandseeru@gmail.com>
2023-12-19 19:52:36 +0300

    adding a function to change the number value of UILabel

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var value = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countLabel.text = "0"
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        value += 1
+        countLabel.text = "\(value)"
+    }
+    
 }
 
